805899099763a0c139407580e6d9da8aad326025
samuelcpr <samuelcpr464@gmail.com>
2022-08-23 16:30:08 -0300

    feat: add publicatins app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginPage from './pages/login';
+
+function App() {
+ return (
+  <div>
+    <LoginPage/>
+  </div>
+ )
+}
+
+export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <h1>Loginpage</h1>;
+};
+
+export default LoginPage;
